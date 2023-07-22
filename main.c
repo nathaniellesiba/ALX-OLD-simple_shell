fb0820d9eb229bd3502894a744186fa26447c26f
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-07-23 00:02:32 +0200

    task 0

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+* betty - proud betty numbers
+* @num1: integer 1 number
+* @add1: integer 1 added
+*
+* Return: the results of betty numbers
+*/
+
+int betty(int num1, int add1)
+{
+return(num1 + add1);
+}
---
+++ shell.h
@@ -0,0 +1,8 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+
+int betty(int num1, int add1);
+
+#endif
