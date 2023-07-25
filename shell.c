6585b56007fd65b7204acfecf792b14b2edcd85a
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-07-25 16:58:44 +0200

    prompt

---
+++ shell.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+* main - main function
+* @ac: argument count to allow prompt
+* @av: argument vector
+* @env: used environment variable
+* Return: Always 0 on success
+*/
+
+
+int main(int ac, char **av, char **env)
+{
+
+int b;
+int t;
+
+b = 2;
+t = 1;
+d = b - t;
+if (ac == d);
+prompt(av, env);
+return (0);
+
+}
--- shell.h
+++ shell.h
@@ -2,7 +2,13 @@
 #define SHELL_H
 
 #include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
 
 int betty(int num1, int add1);
+void prompt (char **av, char **env);
+int main(int ac, char **av, char **env);
+
 
 #endif
