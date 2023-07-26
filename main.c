f5a177597014a1633fe23b70bf06043b6fb421dc
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-07-26 18:42:35 +0200

    trying new

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "shell.h"
+#include <stdio.h>
 
 /**
+* main - betty main
 * betty - proud betty numbers
 * @num1: integer 1 number
 * @add1: integer 1 added
@@ -8,9 +10,15 @@
 * Return: the results of betty numbers
 */
 
-int betty(int num1, int add1)
+int main(void)
+{
+printf("Hello world!.\n");
+}
+
+
+/*int betty(int num1, int add1)
 {
 int sum;
 sum = num1 + add1;
 return (sum);
-}
+}*/
