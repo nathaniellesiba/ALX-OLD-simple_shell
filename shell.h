e84f14e57ffad05b31d78ccea120436379faa343
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-07-26 14:59:37 +0200

    testing

--- shell.h
+++ shell.h
@@ -6,11 +6,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+
+#define MAX_COMMAND_LENGTH 100;
+#define MAX_ARGUMENTS 10
 
 
 int betty(int num1, int add1);
 void prompt (char **av, char **env);
 int main(int ac, char **av, char **env);
+
+
+/*alternative code*/
+void displayPrompt();
+void executeCommand(char* command);
+int main();
+void execute_command(char *command);
+
+
 
 
 #endif
