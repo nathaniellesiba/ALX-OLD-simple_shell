bce5a80274403036bd83a2b43716b3279cb937f5
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-07-26 21:56:35 +0200

    void problem

--- shell.h
+++ shell.h
@@ -17,6 +17,8 @@ void prompt(char **av, char **env);
 int main(int ac, char **av, char **env);
 int exit(info_t *info);
 int main(int ac, char **av);
+void execute_command(char* command);
+int main(void);
 
 
 #endif
